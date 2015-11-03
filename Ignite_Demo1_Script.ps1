e0da7249a887aa6b7e041a91d700ba4c1b6d1347
shzhai <shawnzhai@msn.com>
2015-11-03 15:08:45 +0800

    Ignite Demo

---
+++ Ignite_Demo1_Script.ps1
@@ -0,0 +1,45 @@
+﻿# 这部分仅用作说明
+# 首先提升权限打开Powershell控制台下载在本地虚拟机创建演示虚拟机的脚本，注意其中判断VM集成工具状态脚本如果chcp是936中文，需要修改为"确定"而不是"OK"
+# Get-VMIntegrationService |? Id -match "84EAAE65-2F2E-45F5-9BB5-0E857DC8EB47").PrimaryStatusDescription
+# start-process powershell -Verb runAs
+# Invoke-WebRequest -uri https://aka.ms/newcontainerhost -OutFile New-ContainerHost.ps1
+
+# initilize demo env
+Set-Location $env:HOMEPATH\Work\Ignite2016
+$DemoVMname = "IgniteDemoHost1"
+$cred = Get-Credential -UserName "administrator" -Message "Please enter password"
+$DemoVMPwd = $cred.GetNetworkCredential().Password
+
+# Create Demo windows TP3 VM with windows container
+.\New-ContainerHost.ps1 -VmName $DemoVMname -Password $DemoVMPwd
+
+# Connect Demo VM through VMbus
+Enter-PSSession -VMName $DemoVMname -Credential $cred
+
+# Redirect PS ise to Local 
+# psedit test.ps1
+
+# Assign External vSwitch to Demo VM
+$DemoVM = Get-VM -Name $DemoVMname
+$VMSwitch = Get-VMSwitch
+$DemoVMNetAdaptor = Get-VMNetworkAdapter -VM $DemoVM
+Connect-VMNetworkAdapter -VMNetworkAdapter $DemoVMNetAdaptor[0] -VMSwitch $VMSwitch
+
+# Add Demo VM env to Local Demo Machine
+$DemoVMIP = $DemoVM.NetworkAdapters.IPAddresses[0]
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value $DemoVMIP -Force -Concatenate  
+Get-Item WSMan:\localhost\Client\TrustedHosts
+
+# Create PS Session to Demo VM 
+$s = New-PSSession -ComputerName $DemoVMIP -Credential $cred
+
+# Copy Demo Script to Demo VM
+Invoke-Command -Session $s -ScriptBlock {New-Item -ItemType Directory C:\DemoScripts -Force;Set-Location C:\DemoScripts}
+Copy-Item -ToSession $s -Path .\Demo*Script.ps1 -Destination c:\DemoScripts
+
+
+# Enter PS Session to Demo VM
+Enter-PSSession -Session $s
+
+# 重定向在本地通过PowerShell_ISE打开演示脚本
+PSEdit Demo*Script.ps1
---
+++ Ignite_Demo4_Script.ps1
@@ -0,0 +1,38 @@
+﻿# 初始化演示环境设置设置Nano映像路径，构建BaseVHD路径以及Nano VHD路径
+Set-Location $env:USERPROFILE\Work\Ignite2016
+$BasePath = New-Item -ItemType Directory .\NanoServer -Force | Select-Object -ExpandProperty FullName 
+$ImagePath = 'C:\Users\shzhai.FAREAST\Work\MVA TBD\Windows Server 2016 TP\10514.0.150808-1529.TH2_RELEASE_SERVER_OEMRET_X64FRE_EN-US.ISO'
+if (!(Get-DiskImage -ImagePath $ImagePath | Get-Volume)){ Mount-DiskImage -ImagePath $ImagePath -StorageType ISO -PassThru }
+$MediaPath = "$((Get-DiskImage -ImagePath $ImagePath | Get-Volume).DriveLetter)" + ":\"
+$NanoVMName = 'IgniteNanoVM'
+$VMPath = "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\$NanoVMName"
+if ($CurrentVM = Get-VM -Name $NanoVMName -ErrorAction SilentlyContinue) {
+    if ($CurrentVM.State -ne "Off") {
+        Stop-VM -VM $CurrentVM -TurnOff -Force
+    }
+    Remove-VM -VM $CurrentVM -Force -Verbose
+} 
+Remove-Item -Path $VMPath -Recurse -Force -ErrorAction SilentlyContinue
+
+
+# 准备打包Nano VHD的脚本及运行环境
+Copy-Item $MediaPath\NanoServer\*.ps1 .\ -Force
+
+. .\convert-windowsimage.ps1
+. .\new-nanoserverimage.ps1
+
+# 创建Nano VHD，后期可以客户化（无人值守应答，加入域或静态IP设置等），演示中用到了最简单设置
+New-NanoServerImage -MediaPath $MediaPath -BasePath $BasePath -TargetPath $VMPath `
+-GuestDrivers -ReverseForwarders -ComputerName $NanoVMName -Language 'en-us' `
+-AdministratorPassword ("ignite2016" | ConvertTo-SecureString -AsPlainText -Force) 
+
+
+# Check Nano Server log
+ise 'C:\Users\SHZHAI~1.FAR\AppData\Local\Temp\New-NanoServerImage.log'
+
+# 创建Nano虚拟机
+$NanoVHD = "$VMPath"+"\"+"$NanoVMName"+".vhd"
+$vSwitchName = (Get-VMSwitch).Name
+New-VM -Name $NanoVMName -MemoryStartupBytes 1GB -Generation 1 -VHDPath $NanoVHD -SwitchName $vSwitchName | Start-VM -Passthru
+
+
